46dc8e2813254d2db86ba11f980a1029eceed07d
mogewo <rei.s3110@gmail.com>
2017-04-18 05:17:34 +0000

    pagination

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   
   def index
-    @messages = Message.order(created_at: :desc).page(params[:page]).per(3)
+    @messages = Message.all.page(params[:page]).per(10)
   end
 
   def show
